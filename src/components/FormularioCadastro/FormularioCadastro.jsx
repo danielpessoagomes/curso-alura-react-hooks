346c3dcc86b83c3ca0b67c9ed2719d74faf577c0
Daniel Gomes <danielgomes@dginfope.com.br>
2020-10-05 23:39:06 -0300

    Primeiro commit

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
+
+function FormularioCadastro() {
+    return (
+        <form>
+            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
+            <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
+            <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
+
+            <FormControlLabel label="Promoções" control={<Switch defaultChecked name="promocoes" color="primary" />} />
+            <FormControlLabel label="Novidades" control={<Switch defaultChecked name="novidades" color="primary" />} />
+            
+            <Button variant="contained" color="primary" type="submit">Cadastrar</Button>
+        </form>
+    )
+}
+
+export default FormularioCadastro;
